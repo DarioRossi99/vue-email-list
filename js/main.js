618ee40e25497b2ae14af156da0fb84f1e66946f
DarioRossi99 <rssdra99@gmail.com>
2022-11-30 16:32:08 +0100

    aggiunta bonus

--- js/main.js
+++ js/main.js
@@ -4,23 +4,26 @@ const app = createApp({
 
     data () {
         return {
-            listaEmail: []
+            listaEmail: [],
+            listaTemporanea: [],
         };
     },
     methods:{
         fetchData(){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((resp) => {
-
                 console.log(resp);
-
-                console.log(resp.data)
-
-                this.listaEmail.push(resp.data.response);
+                console.log(resp.data);
+                this.listaTemporanea.push(resp.data.response);
+                this.ajaxCounter++;
+                if (this.ajaxCounter === 10) {
+                    this.listaEmail = this.listaTemporanea;
+                }
             })
         }
     },
     mounted () {
+        this.ajaxCounter = 0;
         for(let i = 0; i < 10; i++){
             this.fetchData();
         }
